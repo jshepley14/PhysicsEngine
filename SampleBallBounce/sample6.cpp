9e6fe9be077962c707caa861b898a9e2397ad118
Joe_Shepley <jshepley14@gmail.com>
2016-06-17 16:50:02 -0400

    falling boxes result in a stack

--- SampleBallBounce/sample6.cpp
+++ SampleBallBounce/sample6.cpp
@@ -82,26 +82,26 @@ const dMatrix3 B4matrix[3][3] = {  { 0, 1, 1},
 */
 
 //Use this for static equilibrium example
+//trying to implement new example, make the boxes stacked and what not
 
-const dReal centerSphr[3] = {S1x=0.222523,S1y=-0.0803006,S1z=0.2};
-const dReal center1[3] = {B1x=0.315066,B1y=0.120175,B1z=0.590846};
-const dReal center2[3] = {B2x=0.305423,B2y=0.600279,B2z=0.25};
-const dReal center3[3] = {B3x=-1.08545,B3y=-0.410903,B3z=0.25};
-const dReal center4[3] = {B4x=1.13042,B4y=0.0812032,B4z=0.5};
+const dReal centerSphr[3] = {S1x=4,S1y=4,S1z=4};
+const dReal center1[3] = {B1x=0.5,B1y=0,B1z=0.5};
+const dReal center2[3] = {B1x=-0.5,B1y=0,B1z=0.5};
+const dReal center3[3] = {B1x=0.5,B1y=0,B1z=1.5};
+const dReal center4[3] = {B1x=-0.5,B1y=0,B1z=1.5};
 
-const dMatrix3 B1matrix[3][3] = {  { -0.0343974,-1.49881e-11,0.997988},
-                            { 0,-0.30751,0.942834},
-                            { -0.00858871,0,-0.930998}  };
-
-const dMatrix3 B2matrix[3][3] = {  { -3.35709e-12,-1.27513e-11,1},
-                            { 0,-6.47146e-13,1},
-                            { 1.27513e-11,0,-1}  };
-const dMatrix3 B3matrix[3][3] = {  { -4.82185e-16,-0.0168371,0.999858},
-                            { 0,-1.73472e-18,0.999858},
-                            { 0.0168371,0,-1}  };
-const dMatrix3 B4matrix[3][3] = {  { 1,-3.61907e-18,6.88306e-20},
-                            { 0,3.61907e-18,1},
-                            { -6.39488e-19,0,-6.88306e-20}  };
+const dMatrix3 B1matrix[3][3] = {  { 0, 1, 1},
+                                { 0, 1, 1},
+                                { 0, 1, 1}  }; 
+const dMatrix3 B2matrix[3][3] = {  { 0, 1, 1},
+                                { 0, 1, 1},
+                                { 0, 1, 1}  }; 
+const dMatrix3 B3matrix[3][3] = {  { 0, 1, 1},
+                                { 0, 1, 1},
+                                { 0, 1, 1}  }; 
+const dMatrix3 B4matrix[3][3] = {  { 0, 1, 1},
+                                { 0, 1, 1},
+                                { 0, 1, 1}  }; 
 
 
 
