1b6a5f8616944101cf2f78ac6b3e2417a1dcd903
Joe_Shepley <jshepley14@gmail.com>
2016-06-24 14:57:51 -0400

    deals with leaning block, added more thorough StEq checker

--- manyShapes.cpp
+++ manyShapes.cpp
@@ -27,8 +27,9 @@ static void printEndpositions(const dReal *Box1pos);
 
 //static equilibrium constants
 static int counter = 0;          //iterator which will reach COUNT
-static int COUNT = 6;           //how long until we want to wait to check satic equlibrium?
+static int COUNT = 30;           //how long until we want to wait to check satic equlibrium?
 static double THRESHHOLD = 0.1;  //how much movement is allowed
+static double GRAVITY = -19.8;   
 static double TIMESTEP = 0.02;    //Time step originally was at 0.01, but we may
                                  //be able to increase this making calculations faster
                                  //most efficient would prob be 0.01 ≤ TIMESTEP ≤ 0.03
@@ -69,41 +70,19 @@ dReal B4x, B4y, B4z; //Box4
 
 
 
-//TITLE: Box is balanced on sphere. Four boxes and a sphere in static equilibrium, 
-//ID: 3
+//....................................................................................................
+//TITLE: leaning block falling.  Example of configuration that requires COUNT to be large OR small Threshold 
+
 //Position declarations
-const dReal centerSphr[3] = {S1x=0.222523,S1y=-0.0803006,S1z=0.2};
-const dReal center1[3] = {B1x=0.315066,B1y=0.120175,B1z=0.590846};
-const dReal center2[3] = {B2x=0.305423,B2y=0.600279,B2z=0.25};
-const dReal center3[3] = {B3x=-1.08545,B3y=-0.410903,B3z=0.25};
-const dReal center4[3] = {B4x=1.13042,B4y=0.0812032,B4z=0.5};
-
-//Rotation declarations
-const dMatrix3 B1matrix = {-0.0343974,-1.49881e-11, 0.997988,
-                                0,          -0.30751, 0.942834,
-                             -0.0085887,        0,   -0.930998  };
-const dMatrix3 B2matrix = { -3.35709e-12,-1.27513e-11,     1,
-                             0,            -6.47146e-13,     1,
-                             1.27513e-11,           0,      -1  };
-const dMatrix3 B3matrix = {-4.82185e-16, -0.0168371,  0.999858,
-                                0,        -1.73472e-18, 0.999858,
-                             0.0168371,          0,          -1  };
-const dMatrix3 B4matrix = {1,           -3.61907e-18, 6.88306e-20,
-                             0,            3.61907e-18,          1,
-                             -6.39488e-19,         0,      -6.88306e-20  };
-
-
-/*
-//Position declarations
-const dReal centerSphr[3] = {S1x=0.0,S1y=0,S1z=3.5}; // length of edges
-const dReal center1[3] = {B1x=0.0,B1y=0.8,B1z=0.5}; // length of edges
-const dReal center2[3] = {B2x=0.0,B2y=-0.8,B2z=0.5}; // length of edges
-const dReal center3[3] = {B3x=-0.8,B3y=0.0,B3z=0.5}; // length of edges
-const dReal center4[3] = {B4x=0.8,B4y=0.0,B4z=0.5}; // length of edges
+const dReal centerSphr[3] = {S1x=-2.0,S1y=0,S1z=0.2}; // length of edges
+const dReal center1[3] = {B1x=0.0,B1y=0,B1z=0.55}; // length of edges
+const dReal center2[3] = {B2x=0,B2y=1,B2z=0.5}; // length of edges
+const dReal center3[3] = {B3x=0,B3y=2,B3z=0.5}; // length of edges
+const dReal center4[3] = {B4x=0,B4y=3,B4z=0.5}; // length of edges
 //Rotation declarations
 const dMatrix3 B1matrix = { 1, 0, 0,
                             0, 0, 0,
-                            0, 0, 0  }; 
+                            0, 0, 0.5  }; 
 const dMatrix3 B2matrix = { 1, 0, 0,
                             0, 0, 0,
                             0, 0, 0  }; 
@@ -113,35 +92,6 @@ const dMatrix3 B3matrix = { 1, 0, 0,
 const dMatrix3 B4matrix = { 1, 0, 0,
                             0, 0, 0,
                             0, 0, 0  }; 
-
-*/
-
-/*
-//Position declarations
-const dReal centerSphr[3] = {S1x=-2.0,S1y=0,S1z=1}; // length of edges
-const dReal center1[3] = {B1x=0,B1y=0,B1z=1.26}; // length of edges
-const dReal center2[3] = {B2x=-0.5,B2y=0,B2z=0.5}; // length of edges
-const dReal center3[3] = {B3x=0.5,B3y=0,B3z=0.5}; // length of edges
-const dReal center4[3] = {B4x=0,B4y=0,B4z=1.8}; // length of edges
-//Rotation declarations
-const dMatrix3 B1matrix = { 0, 0, 1,
-                            0, -1, 0,
-                            1, 0, 0  }; 
-const dMatrix3 B2matrix = { 1, 0, 0,
-                            0, 0, 0,
-                            0, 0, 0  }; 
-const dMatrix3 B3matrix = { 1, 0, 0,
-                            0, 0, 0,
-                            0, 0, 0  }; 
-const dMatrix3 B4matrix = { 1, 0, 0,
-                            0, 0, 0,
-                            0, 0, 0  }; 
-
-*/
-
-
-
-
 
 
 
@@ -181,6 +131,42 @@ static void drawBox( MyObject &box, const dReal* sides){
     #endif
 }
 
+
+
+//This function checks for static equilibrium
+/* Later we could turn this into a bool return. Additionally, one
+*  could feed in dReal types instead of doubles. One would also
+*  want to be able to feed in an arbitrary number of object data.
+*
+*/
+static bool inStaticEquilibrium(const dReal* center, MyObject &object){
+
+    double startX = center[0];
+    double startY = center[1];
+    double startZ = center[2];
+    double endX = dBodyGetPosition(object.body)[0];
+    double endY = dBodyGetPosition(object.body)[1];
+    double endZ = dBodyGetPosition(object.body)[2];
+    double deltaX = std::abs(startX - endX);
+    double deltaY = std::abs(startY - endY);
+    double deltaZ = std::abs(startZ - endZ);
+    
+    //Check if object moved since initialization
+    if ( deltaX > THRESHHOLD || deltaY > THRESHHOLD || deltaZ > THRESHHOLD){
+        cout << "NOT in static equilibrium" << endl;
+        return false;
+    } else{ 
+        cout << "YES: Is in static equilibrium" << endl;
+        return true;
+    }
+    cout<<"          X            Y            Z"  <<endl;
+    cout<<"Start: "<<startX<<", "<<startY<<", "<<startZ<<endl;
+    cout<<"  End: "<<endX<<", "<<endY<<", "<<endZ<<endl;
+    cout<<"Delta: "<<deltaX<<", "<<deltaY<<", "<<deltaZ<<endl;
+}
+
+
+
 static void nearCallback(void *data, dGeomID o1, dGeomID o2)
 {
   const int N = 10;
@@ -216,24 +202,9 @@ static void simLoop (int pause)
   }
 
   const dReal *pos,*R;
-
-  /*
-  const dReal *Box1pos, *Box1R;
-  const dReal *Box2pos, *Box2R;
-  const dReal *Box3pos, *Box3R;
-  */
- 
   dSpaceCollide(space,0,&nearCallback);
   dWorldStep(world,TIMESTEP);
   dJointGroupEmpty(contactgroup);
-
-  // what is this flag stuff?
-  //if (flag == 0) dsSetColor(1.0, 0.0, 0.0);
-  //else           dsSetColor(0.0, 0.0, 1.0);
-  //
-
-
- 
   pos = dBodyGetPosition(ball.body);
   R   = dBodyGetRotation(ball.body);
   
@@ -250,75 +221,42 @@ static void simLoop (int pause)
   dsSetColor(0,12,200);
   drawBox(box4, sides4);   
 #else
-  drawBox(box1, sides1);
-  
+  drawBox(box1, sides1); 
   drawBox(box2, sides2);
-  
   drawBox(box3, sides3);
-  
   drawBox(box4, sides4);
-  
-
   //print the positions and time step or whatever when your not drawing
   //printf("%5d steps x=%.3f y=%.3f z=%.3f \n",(int)step++,pos[0],pos[1],pos[2]);
 #endif
 
   
-
-
   /*
    *  Static Equilibrium Detection
    *  
   */ 
   
   if (counter == COUNT){
-      double zpos = dBodyGetPosition(box1.body)[2];
-      inStaticEquilibrium(B1z, zpos); //feed it in the Zinital and Zfinal coordinates of Box4
+    if(
+      !inStaticEquilibrium(center1, box1) || //feed it in the Zinital and Zfinal coordinates of Box4
+      !inStaticEquilibrium(center2, box2) ||
+      !inStaticEquilibrium(center3, box3) ||
+      !inStaticEquilibrium(center4, box4) ||
+      !inStaticEquilibrium(centerSphr, ball) ){
+        cout<<"NOT in static equilibrium"<<endl;
+      }else{
+        cout<<"YES in static equilibrium"<<endl;
+      }
 
-  //end chrono timer
-  endTime = chrono::steady_clock::now();
-  auto diff = endTime - startTime;
-  cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;
-
-  //seconds
-  //chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(endTime - startTime);
-  //cout << "It took me " << time_span.count() << " seconds.";
+      //end chrono timer
+      endTime = chrono::steady_clock::now();
+      auto diff = endTime - startTime;
+      cout << chrono::duration <double, milli> (diff).count() << " ms" << endl;
+      //seconds
+      //chrono::duration<double> time_span = chrono::duration_cast<chrono::duration<double>>(endTime - startTime);
+      //cout << "It took me " << time_span.count() << " seconds.";
 
 
     }
-
-   
-  /*
-   *  This next block of code prints out all the final positions and rotations
-   *  so that one could paste the results back into the code and get static equilibrium
-   *  instead of doing Box1pos to access the coordinates maybe I should do 
-   *  dBodyGetPosition(box1.body) and dBodyGetRotation(box1.body) so that its generalized.
-  */ 
-
-  /*
-  if (counter == 200) { //at 200 assume objects are at rest
-    cout << "const dReal centerSphr[3] = {S1x="<<pos[0]<<",S1y="<<pos[1]<<",S1z="<<pos[2]<<"};\n" <<endl; // length of edges
-    cout << "const dReal center1[3] = {B1x="<<Box1pos[0]<<",B1y="<<Box1pos[1]<<",B1z="<<Box1pos[2]<<"};\n" <<endl; // length of edges
-    cout << "const dReal center2[3] = {B2x="<<Box2pos[0]<<",B2y="<<Box2pos[1]<<",B2z="<<Box2pos[2]<<"};\n" <<endl; // length of edges
-    cout << "const dReal center3[3] = {B3x="<<Box3pos[0]<<",B3y="<<Box3pos[1]<<",B3z="<<Box3pos[2]<<"};\n" <<endl; // length of edges
-    cout << "const dReal center4[3] = {B4x="<<Box4pos[0]<<",B4y="<<Box4pos[1]<<",B4z="<<Box4pos[2]<<"};\n" <<endl; // length of edges
-    cout<<"const dMatrix3 B1matrix[3][3] = {  { "<<Box1R[0]<<","<<Box1R[1]<<","<<Box1R[2]<<"},"<<endl;
-    cout<<"                            { "<<Box1R[3]<<","<<Box1R[4]<<","<<Box1R[5]<<"},"<<endl;
-    cout<<"                            { "<<Box1R[6]<<","<<Box1R[7]<<","<<Box1R[8]<<"}  };"<<endl;
-    cout<<"const dMatrix3 B2matrix[3][3] = {  { "<<Box2R[0]<<","<<Box2R[1]<<","<<Box2R[2]<<"},"<<endl;
-    cout<<"                            { "<<Box2R[3]<<","<<Box2R[4]<<","<<Box2R[5]<<"},"<<endl;
-    cout<<"                            { "<<Box2R[6]<<","<<Box2R[7]<<","<<Box2R[8]<<"}  };"<<endl;
-    cout<<"const dMatrix3 B3matrix[3][3] = {  { "<<Box3R[0]<<","<<Box3R[1]<<","<<Box3R[2]<<"},"<<endl;
-    cout<<"                            { "<<Box3R[3]<<","<<Box3R[4]<<","<<Box3R[5]<<"},"<<endl;
-    cout<<"                            { "<<Box3R[6]<<","<<Box3R[7]<<","<<Box3R[8]<<"}  };"<<endl;
-   
-    cout<<"const dMatrix3 B4matrix[3][3] = {  { "<<Box4R[0]<<","<<Box4R[1]<<","<<Box4R[2]<<"},"<<endl;
-    cout<<"                            { "<<Box4R[3]<<","<<Box4R[4]<<","<<Box4R[5]<<"},"<<endl;
-    cout<<"                            { "<<Box4R[6]<<","<<Box4R[7]<<","<<Box4R[8]<<"}  };"<<endl;
-   
-  }
- */
-
 
     //Dont forget about the counter!
     counter++;   
@@ -327,26 +265,7 @@ static void simLoop (int pause)
 
 
 
-//This function checks for static equilibrium
-/* Later we could turn this into a bool return. Additionally, one
-*  could feed in dReal types instead of doubles. One would also
-*  want to be able to feed in an arbitrary number of object data.
-*
-*/
-static void inStaticEquilibrium(double startZ, double endZ ){
 
-    double deltaZ = std::abs(startZ - endZ);
-    cout << "StartPosition: " << startZ << " \n" << endl;
-    cout << "EndPosition: " << endZ << " \n" << endl;
-    cout << "delta Z: " << deltaZ << " \n" << endl;
-    cout << "counter = " << COUNT  << " \n" << endl;
-    cout << "THRESHHOLD : " << THRESHHOLD << " \n" << endl;
-    //Check if object moved since initialization
-    if ( (deltaZ) > THRESHHOLD){
-        cout << "FALSE: Not in static equilibrium\n" << endl;
-    } else 
-        cout << "True: Is in static equilibrium\n" << endl;
-}
 
 
 static void printEndpositions(const dReal *Box1pos){
@@ -399,7 +318,7 @@ int main (int argc, char *argv[])
   space = dHashSpaceCreate(0);
   contactgroup = dJointGroupCreate(0);
 
-  dWorldSetGravity(world,0,0,-9.8);
+  dWorldSetGravity(world,0,0,GRAVITY);
 
   // Create a ground
   ground = dCreatePlane(space,0,0,1,0);
