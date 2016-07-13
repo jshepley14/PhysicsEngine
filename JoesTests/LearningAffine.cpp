174e32c87412dcfc088fbe96373e299417ffad99
Joe_Shepley <jshepley14@gmail.com>
2016-07-12 21:12:37 -0400

    makes affine3d and prints whats inside

--- JoesTests/LearningAffine.cpp
+++ JoesTests/LearningAffine.cpp
@@ -15,13 +15,41 @@ Eigen::Affine3d create_rotation_matrix(double ax, double ay, double az) {
 }
 
 int main() {
-  Eigen::Affine3d r = create_rotation_matrix(1.5,0,0);
+  Eigen::Affine3d r = create_rotation_matrix(1.5,2,2);
   Eigen::Affine3d t(Eigen::Translation3d(Eigen::Vector3d(1,1,2)));
 
+
+  
   Eigen::Matrix4d m = (t * r).matrix(); // Option 1
+
+  Eigen::Affine3d a = (r*t); // Option 1
+
+  cout<< a.operator()(0,0)<<"\n";
+  cout<< a.operator()(0,1)<<"\n";
+  cout<< a.operator()(0,2)<<"\n";
+  cout<< a.operator()(0,3)<<"\n";
+  cout<< a.operator()(1,0)<<"\n";
+  cout<< a.operator()(1,1)<<"\n";
+  cout<< a.operator()(1,2)<<"\n";
+  cout<< a.operator()(1,3)<<"\n";
+  cout<< a.operator()(2,0)<<"\n";
+  cout<< a.operator()(2,1)<<"\n";
+  cout<< a.operator()(2,2)<<"\n";
+  cout<< a.operator()(2,3)<<"\n";
+  cout<< a.operator()(3,0)<<"\n";
+  cout<< a.operator()(3,1)<<"\n";
+  cout<< a.operator()(3,2)<<"\n";
+  cout<< a.operator()(3,3)<<"\n";
+
+  
+  //cout<< a.data()[3];
+
+ /*
   cout <<"Matrix4d: \n" <<  m<<"\n";
   cout<<"Rotation \n" << m.block(0,0,3,3)<<"\n";
   cout<<"Vector: \n" << m.block<4,1>(0,3);
+*/
+  
 
   return 0;
 }
